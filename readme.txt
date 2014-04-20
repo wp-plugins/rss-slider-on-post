d258205cd62eef2dc8838294f11961edfe7f948f
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 13:25:54 +0000

    Tested up to 3.9
    git-svn-id: https://plugins.svn.wordpress.org/rss-slider-on-post/trunk@898678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,10 +3,10 @@ Contributors: www.gopiplus.com, gopiplus
 Donate link: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
 Author URI: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
 Plugin URI: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
-Tags:  Rss, plugin, wordpress, slider
+Tags:  rss, feed, slider
 Requires at least: 3.2
-Tested up to: 3.8
-Stable tag: 6.1
+Tested up to: 3.9
+Stable tag: 6.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 	
@@ -89,6 +89,11 @@ Option to add the scroll directly in to the theme using PHP code.
 2. Now this plugin supports localization (or internationalization). i.e. option to translate into other languages. 
 Plugin *.po file (rss-slider-on-post.po) available in the languages folder.
 
+= 6.2 =
+
+1. Tested up to 3.9
+2. Now it using fetch_feed() wordpress method to load rss feed. (fetch_feed() uses the SimplePie and FeedCache functionality for retrieval and parsing and automatic caching)
+
 == Changelog ==
 
 = 1.0 =
@@ -128,3 +133,8 @@ Option to add the scroll directly in to the theme using PHP code.
 1. Tested up to 3.8
 2. Now this plugin supports localization (or internationalization). i.e. option to translate into other languages. 
 Plugin *.po file (rss-slider-on-post.po) available in the languages folder.
+
+= 6.2 =
+
+1. Tested up to 3.9
+2. Now it using fetch_feed() wordpress method to load rss feed. (fetch_feed() uses the SimplePie and FeedCache functionality for retrieval and parsing and automatic caching)
--- rss-slider-on-post.php
+++ rss-slider-on-post.php
@@ -3,11 +3,11 @@
 Plugin Name: Rss slider on post
 Plugin URI: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
 Description: RSS slider on post is a small WordPress plugin to create the scroller/slider text gallery into the posts and pages, that makes rss integration to your web site very easy. In the admin we have option to add the rss feed link.
-Author: Gopi.R
-Version: 6.1
+Author: Gopi Ramasamy
+Version: 6.2
 Author URI: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
 Donate link: http://www.gopiplus.com/work/2012/04/01/rss-slider-on-post-wordpress-plugin/
-Tags: Rss, plugin, wordpress, slider
+Tags: rss, feed, slider
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -39,7 +39,7 @@ function rssslider_admin_options()
 {
 	global $wpdb;
 	?>
-		<div class="wrap">
+	<div class="wrap">
 	  <div class="form-wrap">
 		<div id="icon-plugins" class="icon32 icon32-posts-post"></div>
 		<?php
@@ -255,128 +255,128 @@ function rssslider_shortcode( $atts )
 	$content = @file_get_contents($url);
 	if (strpos($http_response_header[0], "200")) 
 	{ 
-		$f = fopen( $url, 'r' );
-		while( $data = fread( $f, 4096 ) ) 
+		$cnt = 0;
+		$maxitems = 0;
+		$rssslider_count = 0;
+		$rssslider_html = "";
+		$IRjsjs = "";
+		$rssslider_x = "";
+		include_once( ABSPATH . WPINC . '/feed.php' );
+		$rss = fetch_feed( $url );
+		if ( ! is_wp_error( $rss ) )
 		{
-			$xml .= $data; 
-		}
-		fclose( $f );
-		preg_match_all( "/\<item\>(.*?)\<\/item\>/s", $xml, $itemblocks );
-	
-		if ( ! empty($itemblocks) ) 
-		{
-			$rssslider_count = 0;
-			$rssslider_html = "";
-			$IRjsjs = "";
-			$rssslider_x = "";
-			foreach( $itemblocks[1] as $block )
+			$cnt = 0;
+			$maxitems = $rss->get_item_quantity( 10 ); 
+			$rss_items = $rss->get_items( 0, $maxitems );
+			if ( $maxitems > 0 )
 			{
-				$rssslider_target = "_blank";
-				
-				preg_match_all( "/\<title\>(.*?)\<\/title\>/",  $block, $title );
-				preg_match_all( "/\<link\>(.*?)\<\/link\>/", $block, $link );
-				preg_match_all( "/\<description\>(.*?)\<\/description\>/", $block, $description );
-				
-				$rssslider_title = $title[1][0];
-				$rssslider_title = mysql_real_escape_string(trim($rssslider_title));
-				$rssslider_link = $link[1][0];
-				$rssslider_link = trim($rssslider_link);
-				$rssslider_text = $description[1][0];
-				$rssslider_text = str_replace("&lt;![CDATA[","",$rssslider_text);
-				$rssslider_text = str_replace("<![CDATA[","",$rssslider_text);
-				$rssslider_text = str_replace("]]&gt;","",$rssslider_text);
-				$rssslider_text = str_replace("]]>","",$rssslider_text);
-				
-				if(is_numeric($rssslider_textlength))
+				foreach ( $rss_items as $item )
 				{
-					if($rssslider_textlength <> "" && $rssslider_textlength > 0 )
+					$rssslider_link = $item->get_permalink();
+					$rssslider_title = esc_sql($item->get_title());
+					$rssslider_text = esc_sql($item->get_description());
+					
+					$rssslider_target = "_blank";							
+					$rssslider_text = strip_tags(strip_shortcodes($rssslider_text));
+					$words = explode(' ', $rssslider_text, $rssslider_textlength + 1);
+					if(count($words) > $rssslider_textlength)
 					{
-						$rssslider_text = substr($rssslider_text, 0, $rssslider_textlength);
+						array_pop($words);
+						array_push($words, '...');
+						$rssslider_text = implode(' ', $words);
 					}
-				}
-				
-				$rssslider_scrollheights = $rssslider_scrollheight."px";	
-				
-				$rssslider_html = $rssslider_html . "<div class='rssslider_div' style='height:".$rssslider_scrollheights.";padding:1px 0px 1px 0px;'>"; 
-				
-				if($rssslider_title <> "" )
-				{
-					$rssslider_html = $rssslider_html . "<div style='padding-left:4px;'><strong>";	
-					$IRjsjs = $IRjsjs . "<div style=\'padding-left:4px;\'><strong>";				
-					if($rssslider_link <> "" ) 
-					{ 
-						$rssslider_html = $rssslider_html . "<a href='$rssslider_link'>"; 
-						$IRjsjs = $IRjsjs . "<a href=\'$rssslider_link\'>";
-					} 
-					$rssslider_html = $rssslider_html . $rssslider_title;
-					$IRjsjs = $IRjsjs . $rssslider_title;
-					if($rssslider_link <> "" ) 
-					{ 
-						$rssslider_html = $rssslider_html . "</a>"; 
-						$IRjsjs = $IRjsjs . "</a>";
+					$rssslider_text = nl2br($rssslider_text);
+					$rssslider_text = str_replace("<br>", " ", $rssslider_text);
+					$rssslider_text = str_replace("<br />", " ", $rssslider_text);
+					$rssslider_text = str_replace("\r\n", " ", $rssslider_text);
+					
+					$rssslider_scrollheights = $rssslider_scrollheight."px";		
+					$rssslider_html = $rssslider_html . "<div class='rssslider_div' style='height:".$rssslider_scrollheights.";padding:1px 0px 1px 0px;'>"; 
+					
+					if($rssslider_title <> "" )
+					{
+						$rssslider_html = $rssslider_html . "<div style='padding-left:4px;'><strong>";	
+						$IRjsjs = $IRjsjs . "<div style=\'padding-left:4px;\'><strong>";				
+						if($rssslider_link <> "" ) 
+						{ 
+							$rssslider_html = $rssslider_html . "<a href='$rssslider_link'>"; 
+							$IRjsjs = $IRjsjs . "<a href=\'$rssslider_link\'>";
+						} 
+						$rssslider_html = $rssslider_html . $rssslider_title;
+						$IRjsjs = $IRjsjs . $rssslider_title;
+						if($rssslider_link <> "" ) 
+						{ 
+							$rssslider_html = $rssslider_html . "</a>"; 
+							$IRjsjs = $IRjsjs . "</a>";
+						}
+						$rssslider_html = $rssslider_html . "</strong></div>";
+						$IRjsjs = $IRjsjs . "</strong></div>";
 					}
-					$rssslider_html = $rssslider_html . "</strong></div>";
-					$IRjsjs = $IRjsjs . "</strong></div>";
+					
+					if($rssslider_text <> "" )
+					{
+						$rssslider_html = $rssslider_html . "<div style='padding-left:4px;'>$rssslider_text</div>";	
+						$IRjsjs = $IRjsjs . "<div style=\'padding-left:4px;\'>$rssslider_text</div>";	
+					}
+					
+					$rssslider_html = $rssslider_html . "</div>";
+					
+					$rssslider_x = $rssslider_x . "rssslider[$rssslider_count] = '<div class=\'rssslider_div\' style=\'height:".$rssslider_scrollheights.";padding:1px 0px 1px 0px;\'>$IRjsjs</div>'; ";	
+					$rssslider_count++;
+					$IRjsjs = "";
+					$cnt++;
 				}
 				
-				if($rssslider_text <> "" )
+				
+				$rssslider_scrollheight = $rssslider_scrollheight + 4;
+				if($rssslider_count >= $rssslider_sametimedisplay)
 				{
-					$rssslider_html = $rssslider_html . "<div style='padding-left:4px;'>$rssslider_text</div>";	
-					$IRjsjs = $IRjsjs . "<div style=\'padding-left:4px;\'>$rssslider_text</div>";	
+					$rssslider_count = $rssslider_sametimedisplay;
+					$rssslider_scrollheight_New = ($rssslider_scrollheight * $rssslider_sametimedisplay);
 				}
-				
-				$rssslider_html = $rssslider_html . "</div>";
-				
-				$rssslider_x = $rssslider_x . "rssslider[$rssslider_count] = '<div class=\'rssslider_div\' style=\'height:".$rssslider_scrollheights.";padding:1px 0px 1px 0px;\'>$IRjsjs</div>'; ";	
-				$rssslider_count++;
-				$IRjsjs = "";
-			}
-			
-			$rssslider_scrollheight = $rssslider_scrollheight + 4;
-			if($rssslider_count >= $rssslider_sametimedisplay)
-			{
-				$rssslider_count = $rssslider_sametimedisplay;
-				$rssslider_scrollheight_New = ($rssslider_scrollheight * $rssslider_sametimedisplay);
+				else
+				{
+					$rssslider_count = $rssslider_count;
+					$rssslider_scrollheight_New = ($rssslider_count  * $rssslider_scrollheight);
+				}
+		
+				$rssslider = $rssslider . '<div style="padding-top:8px;padding-bottom:8px;">';
+				$rssslider = $rssslider . '<div style="text-align:left;vertical-align:middle;text-decoration: none;overflow: hidden; position: relative; margin-left: 3px; height: '. @$rssslider_scrollheight .'px;" id="RSSSlider">'.@$rssslider_html.'</div>';
+				$rssslider = $rssslider . '</div>';
+				$rssslider = $rssslider . '<script type="text/javascript">';
+				$rssslider = $rssslider . 'var rssslider = new Array();';
+				$rssslider = $rssslider . "var objrssslider	= '';";
+				$rssslider = $rssslider . "var rssslider_scrollPos 	= '';";
+				$rssslider = $rssslider . "var rssslider_numScrolls	= '';";
+				$rssslider = $rssslider . 'var rssslider_heightOfElm = '. @$rssslider_scrollheight. ';';
+				$rssslider = $rssslider . 'var rssslider_numberOfElm = '. @$rssslider_count. ';';
+				$rssslider = $rssslider . "var rssslider_scrollOn 	= 'true';";
+				$rssslider = $rssslider . 'function rsssliderScroll() ';
+				$rssslider = $rssslider . '{';
+				$rssslider = $rssslider . @$rssslider_x;
+				$rssslider = $rssslider . "objrssslider	= document.getElementById('RSSSlider');";
+				$rssslider = $rssslider . "objrssslider.style.height = (rssslider_numberOfElm * rssslider_heightOfElm) + 'px';";
+				$rssslider = $rssslider . 'rsssliderContent();';
+				$rssslider = $rssslider . '}';
+				$rssslider = $rssslider . '</script>';
+				$rssslider = $rssslider . '<script type="text/javascript">';
+				$rssslider = $rssslider . 'rsssliderScroll();';
+				$rssslider = $rssslider . '</script>';
 			}
 			else
 			{
-				$rssslider_count = $rssslider_count;
-				$rssslider_scrollheight_New = ($rssslider_count  * $rssslider_scrollheight);
+				$rssslider = "No records found.";
 			}
-	
-			$rssslider = $rssslider . '<div style="padding-top:8px;padding-bottom:8px;">';
-			$rssslider = $rssslider . '<div style="text-align:left;vertical-align:middle;text-decoration: none;overflow: hidden; position: relative; margin-left: 3px; height: '. @$rssslider_scrollheight .'px;" id="RSSSlider">'.@$rssslider_html.'</div>';
-			$rssslider = $rssslider . '</div>';
-			$rssslider = $rssslider . '<script type="text/javascript">';
-			$rssslider = $rssslider . 'var rssslider = new Array();';
-			$rssslider = $rssslider . "var objrssslider	= '';";
-			$rssslider = $rssslider . "var rssslider_scrollPos 	= '';";
-			$rssslider = $rssslider . "var rssslider_numScrolls	= '';";
-			$rssslider = $rssslider . 'var rssslider_heightOfElm = '. @$rssslider_scrollheight. ';';
-			$rssslider = $rssslider . 'var rssslider_numberOfElm = '. @$rssslider_count. ';';
-			$rssslider = $rssslider . "var rssslider_scrollOn 	= 'true';";
-			$rssslider = $rssslider . 'function rsssliderScroll() ';
-			$rssslider = $rssslider . '{';
-			$rssslider = $rssslider . @$rssslider_x;
-			$rssslider = $rssslider . "objrssslider	= document.getElementById('RSSSlider');";
-			$rssslider = $rssslider . "objrssslider.style.height = (rssslider_numberOfElm * rssslider_heightOfElm) + 'px';";
-			$rssslider = $rssslider . 'rsssliderContent();';
-			$rssslider = $rssslider . '}';
-			$rssslider = $rssslider . '</script>';
-			$rssslider = $rssslider . '<script type="text/javascript">';
-			$rssslider = $rssslider . 'rsssliderScroll();';
-			$rssslider = $rssslider . '</script>';
 		}
-		else
-		{
-			$rssslider = "No records found.";
+		else 
+		{ 
+			$rssslider = "RSS url is invalid or broken";
 		}
-	} 
+	}
 	else 
 	{ 
 		$rssslider = "RSS url is invalid or broken";
 	}
-
 	return $rssslider;
 }
 
